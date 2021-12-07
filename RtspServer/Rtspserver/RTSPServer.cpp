12e668741137e27325bc210fa4ed67b8683b9bbf
xiaopei.liu <xiaopei.liu@aorise.org>
2021-12-07 16:46:20 +0800

    11111

--- RtspServer/Rtspserver/RTSPServer.cpp
+++ RtspServer/Rtspserver/RTSPServer.cpp
@@ -7,7 +7,7 @@ RTSPServer::RTSPServer(TCPServer* tcpServer)
 {
 	pTcpServer = tcpServer;
 	pTcpServer->SetConnectCallback(incomingConnectionHandlerRTSPEx, this);
-//ffffffffffffffffffffffffff1
+//ffffffffffffffffffffffffff1gfffff
 }
 
 RTSPServer* RTSPServer::createNew(u_int32_t rtspPort, const char* localIpAddress)
